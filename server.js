1a15bbef6d40dc479c79fcec9b54e8571ad39267
harrychou <harrychou@gmail.com>
2012-08-29 20:00:21 -0400

    push from home computer

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.port || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from Harry ... \n');
+  res.end('Hello Azure from Harry ... \n');
 }).listen(port);
